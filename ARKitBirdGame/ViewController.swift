97b38c9f2b2da78a30e1d293bcf46002645a6727
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-02-07 14:37:53 +0000

    Initial Commit

---
+++ ARKitBirdGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKitBirdGame
+//
+//  Created by Rachel Saunders on 07/02/2020.
+//  Copyright © 2020 Rachel Saunders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
